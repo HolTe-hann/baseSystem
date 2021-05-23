d54d964f10703ecc8bb9724489a79894a35a85d8
HolTe-hann <bingbing56781764@163.com>
2021-05-23 22:27:10 +0800

    添加默认主页

---
+++ src/main/java/com/project/base/systemManage/common/index/IndexView.java
@@ -0,0 +1,19 @@
+package com.project.base.systemManage.common.index;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class IndexView extends WebMvcConfigurerAdapter{
+
+    public static String homePage = "/ui/static/index.html";
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:"+homePage);
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    } 
+}
